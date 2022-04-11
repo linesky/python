c74220b1997b7a041d839c71ade7c2aff74864d4
sky line <32512284+linesky@users.noreply.github.com>
2022-04-11 03:31:05 +0100

    python attr.py

---
+++ attr.py
@@ -0,0 +1,11 @@
+def prints(values):
+	print values["name"]
+	print values["addresss"]
+	print values["age"]
+	
+list1=["name","address","age"]
+a={"names":"","addresss":"","age":""}
+a["name"]=list1[0]
+a["addresss"]=list1[1]
+a["age"]=list1[2]
+prints(a)
